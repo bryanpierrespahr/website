f150f4ef0ad42ec25c6e529bd25d91b6a505fdd9
bryanpierrespahr <bryan.spahr@myy.haaga-helia.fi>
2019-02-15 10:45:02 +0100

    Stats per question

--- src/components/QuestionQuiz.js
+++ src/components/QuestionQuiz.js
@@ -25,18 +25,24 @@ class QuestionQuiz extends Component {
     render() {
 
         if (this.state.ready) {
+
             const question = this.state.question;
+            const nbCorrect = question.nbCorrect;
+            const nbIncorrect = question.nbIncorrect;
+            const answered = nbCorrect+nbIncorrect;
+            const correctRate = nbCorrect / answered * 100;
+
 
             return (
                 <div>
                     <div className="row">
                         <div className="col-md-2">
-                            <h5 className="text-left paddingTop10px">Question {this.props.index+1}</h5>
+                            <h5 className="text-left paddingTop10px">Question {this.props.index + 1}</h5>
                         </div>
-                        <div className="col-md-10">
+                        <div className="col-md-8">
                             <div className="row">
                                 <div className="col-md-12">
-                                    <h5 className="text-left paddingTop10px">{question.title}</h5>
+                                    <h5 className="text-left paddingTop10px">{question.question}</h5>
                                 </div>
                             </div>
                             <div className="row">
@@ -71,7 +77,35 @@ class QuestionQuiz extends Component {
                                         <span className="questionAnswer">{question.incorrectAnswers[2]}</span>
                                     </div>
                                 </div>
-
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <h5 className="text-left paddingTop10px">Statistics :</h5>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="text-left">
+                                        <span className="questionAnswer">Answered : {answered}</span>
+                                    </div>
+                                </div>
+                                <div className="col-md-12">
+                                    <div className="text-left">
+                                        <span className="questionAnswer">Correct : {nbCorrect}</span>
+                                    </div>
+                                </div>
+                                <div className="col-md-12">
+                                    <div className="text-left">
+                                        <span className="questionAnswer">Incorrect : {nbIncorrect}</span>
+                                    </div>
+                                </div>
+                                <div className="col-md-12">
+                                    <div className="text-left">
+                                        <span className="questionAnswer">Correct rate : {correctRate} %</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
